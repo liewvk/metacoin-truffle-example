b562c14c5e783349250e53794a30ffedf212308b
liewvk <voonkiong@gamil.com>
2026-03-14 12:31:42 +0800

    Initial commit: MetaCoin Truffle project

---
+++ contracts/ConvertLib.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier:  GPL-3.0
+pragma solidity ^0.8.19;
+
+
+library ConvertLib{
+	function convert(uint amount,uint conversionRate) public pure returns (uint convertedAmount)
+	{
+		return amount * conversionRate;
+	}
+}
---
+++ truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    dashboard: {
+      port: 24012,
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.19"
+    }
+  }
+};
+
